9f03334a57712da440b055942a7bb47de1df4d8d
Pedro <pedroh.olives@gmail.com>
2022-10-30 01:29:00 -0400

    update

--- src/services/websocket/websocket.js
+++ src/services/websocket/websocket.js
@@ -3,7 +3,6 @@ import Publish from "./Events/Publish";
 import Subscribe from "./Events/Subscribe";
 
 export default function initWebSocket(){
-
     let ids = store.getColumn('robots','public_id');
     const publish = new Publish()
     const subscribe = new Subscribe()
