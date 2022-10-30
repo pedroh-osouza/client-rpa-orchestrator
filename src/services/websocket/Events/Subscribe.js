fff0dfa7da2d7217e85aa44ed0c58792c875e365
Pedro <pedroh.olives@gmail.com>
2022-10-30 00:18:40 -0400

    add: notificação start e stop

--- src/services/websocket/Events/Subscribe.js
+++ src/services/websocket/Events/Subscribe.js
@@ -1,3 +1,4 @@
+import initTray from '@/config/tray';
 import ws from '../connection';
 import start from '../triggers/start';
 import stop from '../triggers/stop';
@@ -6,12 +7,16 @@ export default class Subscribe {
     eventStart(public_id) {
         ws.onEvent(`start.${public_id}`, () => {
             start(public_id)
+            let tray = initTray()
+            tray.displayBalloon({ title: 'Robô Iniciado', content: `O Robô ${public_id} foi iniciado` })
         })
     }
 
     eventStop(public_id) {
         ws.onEvent(`stop.${public_id}`, () =>{
             stop()
+            let tray = initTray()
+            tray.displayBalloon({ title: 'Robô Finalizado', content: `O Robô ${public_id} foi finalizado` })
         })
     }
 }
