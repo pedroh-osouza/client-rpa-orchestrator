509b3a38d829077decb7019b581a3115878ee75c
Lucas Mota <ti12@viacometa.com.br>
2022-11-03 17:40:03 -0400

    refact: status

--- src/services/websocket/Events/Publish.js
+++ src/services/websocket/Events/Publish.js
@@ -1,23 +1,24 @@
 /* eslint-disable */
-const axios = require('axios').default;
-var execSync = require('child_process').execSync;
+const axios = require("axios").default;
+var execSync = require("child_process").execSync;
 
 export default class Publish {
-    async status(id) {
-        let uipathRunning = false
-        try {
-            while (true) {
-                var processes = execSync('wmic process get description').toString();
-                uipathRunning = processes.includes('UiPath.Executor');
+  async status(id) {
+    let uipathRunning = false;
 
-                await axios.post(`${process.env.VUE_APP_ROOT_PUB}/status/${id}`, {
-                    in_execution: uipathRunning
-                })
+    while (true) {
+      try {
+        var processes = execSync("wmic process get description").toString();
+        uipathRunning = processes.includes("UiPath.Executor");
 
-                await new Promise(resolve => setTimeout(resolve, 3500));
-            }
-        } catch (e) {
-            console.log('publish status error:', e)
-        }
+        await axios.post(`${process.env.VUE_APP_ROOT_PUB}/status/${id}`, {
+          in_execution: uipathRunning,
+        });
+
+        await new Promise((resolve) => setTimeout(resolve, 3500));
+      } catch (e) {
+        console.log("publish status error:", e);
+      }
     }
+  }
 }
